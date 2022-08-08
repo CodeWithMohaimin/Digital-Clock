4b6bd9342d7f76e510953c04c7d4bbe8722b8857
programmermohaimin <you@example.com>
2022-08-08 12:39:01 +0600

    Digital Clock is ready

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 
+let hour = document.getElementById('hour');
+let minute = document.getElementById('minute');
+let second = document.getElementById('second');
+let progress = document.getElementById('progress');
+
+
+function showCurrentTime() {
+    let date = new Date();
+    let hr = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hour.innerText = hr;
+    minute.innerText = min;
+    second.innerText = sec;
+
+    progress.style.width = (sec /60)*100 +'%'
+}
+
+setInterval(showCurrentTime, 1000)
