259878e163a646331221ffc265932963bd1c0ab4
programmermohaimin <you@example.com>
2022-08-15 07:33:17 +0600

    mastering

--- app.js
+++ app.js
@@ -19,3 +19,52 @@ function showCurrentTime() {
 }
 
 setInterval(showCurrentTime, 1000)
+
+
+
+// my practice is at the bottom
+
+// let hour = document.getElementById('hour');
+// let minute = document.getElementById('minute');
+// let second = document.getElementById('second');
+// let progress = document.getElementById('progress');
+
+
+// function showCurrentTime() {
+//     let date = new Date();
+//     let hr = date.getHours();
+//     let min = date.getMinutes();
+//     let sec = date.getSeconds();
+
+//     hour.innerText = hr;
+//     minute.innerText = min;
+//     second.innerText = sec;
+
+//     progress.style.width = (sec /60)*100 +'%'
+// }
+
+// setInterval(showCurrentTime, 1000)
+
+
+//another practice 
+
+// let hour = document.getElementById('hour');
+// let minute = document.getElementById('minute');
+// let second = document.getElementById('second');
+// let progress = document.getElementById('progress');
+
+
+// function showCurrentTime() {
+//     let date = new Date();
+//     let hr = date.getHours();
+//     let min = date.getMinutes();
+//     let sec = date.getSeconds();
+
+//     hour.innerText = hr;
+//     minute.innerText = min;
+//     second.innerText = sec;
+
+//     progress.style.width = (sec /60)*100 +'%'
+// }
+
+// setInterval(showCurrentTime, 1000)
